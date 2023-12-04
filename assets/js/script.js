0f690f826c7c2a6972848da668dfa4cad973ad2e
Nathan Mota <nathansmota@gmail.com>
2023-12-04 19:21:29 -0300

    :lipstick: feat: Responsividade no mobile menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,18 @@
-const hamburger = document.querySelector('.icon');
-const nav = document.querySelector('.nav-bar');
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburguerIcon = document.getElementById("hamburguerIcon");
+    const navBar = document.querySelector(".nav-bar");
 
-hamburger.addEventListener('click', () => {
-    nav.classList.toggle('active');
+    hamburguerIcon.addEventListener("click", function () {
+        const iconImage = hamburguerIcon.querySelector('img');
+
+        if (hamburguerIcon.classList.contains("open")) {
+            hamburguerIcon.classList.remove("open");
+            iconImage.src = "assets/img/menu_white_36dp.svg";
+        } else {
+            hamburguerIcon.classList.add('open');
+            iconImage.src = "assets/img/close_white_36dp.svg";
+        }
+
+        navBar.classList.toggle("show-nav-bar");
+    });
 });
